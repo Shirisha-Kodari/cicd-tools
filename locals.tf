91c9c52ffd401ca727375e8b886c8d00cceedb98
SHIRISHA KODARI <shirishakodari35@gmail.com>
2026-08-14 20:12:50 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals{
     ami_id = data.aws_ami.joindevops.id   
-    common_tags = {
+    common_tags = { 
         Project = var.project
         Environment = var.environment       
         Terraform = "true"   
